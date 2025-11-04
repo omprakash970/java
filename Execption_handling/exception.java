dfd4ff2a49fc5102c4f98246b48ac298225c5142
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-04 07:30:41 +0530

    done

---
+++ Execption_handling/exception.java
@@ -0,0 +1,12 @@
+package Execption_handling;
+
+public class exception {
+    public static void mmain(String args[]){
+        int m=10; 
+        int n=0;
+        int ans=n/m; 
+        System.out.println("The answer is: "+ans);
+    }
+
+    
+}
---
+++ OOP/Problems_on_oop/problem3.java
@@ -0,0 +1,22 @@
+package OOP.Problems_on_oop;
+
+// Problem 3:
+// Create a class BankAccount with:
+
+// static variable bankName
+
+// instance variables accountNumber, balance
+
+// a static method changeBankName()
+
+// a method displayAccount()
+
+// In main:
+
+// Create 2 accounts, print details, change bank name using static method, and print again.
+
+// 💡 Hint: You’ll see how static affects all objects simultaneously.
+
+public class problem3 {
+
+}
