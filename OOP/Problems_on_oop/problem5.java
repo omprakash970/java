a8b5725019a73ab90698aeccaba2f66060a98171
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-06 22:06:10 +0530

    Done

---
+++ OOP/Problems_on_oop/problem5.java
@@ -0,0 +1,35 @@
+package OOP.Problems_on_oop;
+
+public class Employee {
+    private double salary;
+
+    Employee(double salary) {
+        this.salary = salary;
+    }
+
+    public double getsalary() {
+        return salary;
+    }
+}
+
+class Manager extends Employee {
+    private double bonus;
+
+    Manager(double salary, double bonus) {
+        super(salary);
+        this.bonus = bonus;
+    }
+
+    public double getTotalSalary() {
+        return getsalary() + bonus;
+    }
+
+}
+
+public class problem5 {
+    public static void main(String[] args) {
+        Manager manager = new Manager(50000, 10000);
+        System.out.println("Total Salary of Manager: " + manager.getTotalSalary());
+    }
+
+}
