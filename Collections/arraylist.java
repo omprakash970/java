b626ea89c96778d698e5997ba041e63b0b60aef2
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-11 16:18:47 +0530

    added

--- Collections/arraylist.java
+++ Collections/arraylist.java
@@ -1,17 +1,19 @@
 import java.util.ArrayList;
-
+import java.util.LinkedList;
 class arraylist{
     public static void main(String[] args) {
-        ArrayList<Integer>list = new ArrayList<>(); 
-
-        list.add(1); 
-        list.add(2);
-        list.add(3);
-        list.set(1,5);
-        list.remove(0);
+        LinkedList <Integer> list = new LinkedList<>(); 
+        list.add(10); 
+        list.add(20); 
+        list.add(25); 
+        list.add(30); 
+        
+        list.removeLast();
+        list.addFirst(5);
+        list.remove(Integer.valueOf(10)); 
         for(int i: list){
             System.out.println(i);
-            
         }
+
     }
 }
