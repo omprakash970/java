3d66cc6b49cb5ef4f63c6ae489d38e5cdc7e5e51
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-11 19:10:40 +0530

    done

--- functional_programming/stream_api.java
+++ functional_programming/stream_api.java
@@ -1,18 +1,15 @@
 package functional_programming;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 public class stream_api {
     public static void main(String[] args){
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
-        ArrayList<Integer> evens = numbers.stream().filter(x->x%2==0).collect(Collectors.toCollection(ArrayList::new));
-        ArrayList<Integer> Squares= numbers.stream().map(x->x*x).collect(Collectors.toCollection(ArrayList::new));
-        System.out.println("Even Numbers: "+evens);
-        System.out.println("Squares: "+Squares);
-        
-
-
+       ArrayList<Integer>numbers= new ArrayList<>(Arrays.asList(2, 38, 23, 42)); 
+       List<Integer>EvenNumbers=numbers.stream().filter(x->x%2==0).collect(Collectors.toList());
+       List<Integer>OddNumbers=numbers.stream().filter(x->x%2!=0).collect(Collectors.toList());
+       System.out.println("Even Numbers: "+EvenNumbers);
+       System.out.println("Odd Numbers: "+OddNumbers);
     }
-    
+
 }
