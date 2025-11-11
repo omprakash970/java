9bd57a2a9e8423e859ad4bae150203e37ea0f393
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-11 20:41:43 +0530

    done

--- Threads/thread.java
+++ Threads/thread.java
@@ -1,5 +1,19 @@
 package Threads;
+import java.lang.Thread;
+class Mythread extends Thread{
+    public void run(){
+        for(int i=0;i<10;i++){
+
+            System.out.println("Thread is running");
+        }
+    }
+}
 
 public class thread {
+    public static void main(String[] args){
+        Mythread t1= new Mythread();
+        t1.start();
+    }
+
     
 }
