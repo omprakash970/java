3af606cbcef15478519fce66d3c23bfe34705afe
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-12 09:49:01 +0530

    done

--- Synchronization/deadlock.java
+++ Synchronization/deadlock.java
@@ -1,11 +1,54 @@
 package Synchronization;
 
-class A{
-    synchronization void foo(B b){
+import java.sql.ShardingKey;
 
+class SharedResource{
+    synchronized void methodA(SharedResource sr){
+        System.out.println(Thread.currentThread().getName()+" locked resources A");
+        try{
+            Thread.sleep(100);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName()+" waiting for resources B");
+        synchronized (sr){
+            System.out.println(Thread.currentThread().getName()+" locked resources B");
+        }
     }
+    synchronized void methodB(SharedResource sr){
+        System.out.println("Thread.currentThread().getName()+\" locked resources B\"");
+        try{
+            Thread.sleep(100);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName()+" Waiting for resources B");
+        synchronized (sr){
+            System.out.println(Thread.currentThread().getName()+" locked resources A");
+        }
+    }
+
 }
 
+
+
+
 public class deadlock {
+    public static void main(String[] args){
+        SharedResource sr1=new SharedResource();
+        SharedResource sr2=new SharedResource();
+        Thread t1=new Thread(){
+            public void run(){
+                sr1.methodA(sr2);
+            }
+        };
+        Thread t2=new Thread(){
+            public void run(){
+                sr2.methodB(sr1);
+            }
+        };
+        t1.start();
+        t2.start();
+    }
 
 }
