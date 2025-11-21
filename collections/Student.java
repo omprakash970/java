1e0e55f373a731baa848942899c0f9749277752f
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-21 22:47:45 +0530

    done

--- collections/Student.java
+++ collections/Student.java
@@ -1,35 +1,29 @@
 package collections; 
-import java.util.LinkedList;
-import java.util.Comparator;
-import java.util.ArrayList;
-import java.util.Queue;
-public class Student{
+import java.util.*;
+import java.util.ArrayList; 
+import java.util.Collections;
+class Students{
     int id; 
-    String name;
+    String name; 
     int marks; 
-    Student(int id, String name, int marks){
+    Students(int id, String name, int marks){
         this.id=id; 
-        this.name=name;
-        this.marks=marks;
+        this.name = name; 
+        this.marks = marks; 
     }
-
 }
-class sortByMarks implements Comparator<Student>{
-    public int compare(Student a, Student b){
-        return a.name.compareTo(b.name); 
+class SortByMarks implements Comparator<Students>{
+    public int compare(Students s1, Students s2){
+        return s1.marks-s2.marks; 
     }
 }
 public class Student{
     public static void main(String[] args){
-        ArrayList<Student> list = new ArrayList<>(); 
-         list.add(new Student(1, "Om", 90));
-            list.add(new Student(2, "Nancy", 85));
-            list.add(new Student(3, "Dynamo", 95));
-           collection.sort(list, new sortByMarks()); 
-           System.out.println("After Sorting:");
-            System.out.println(list);   
+        ArrayList<Students>list = new ArrayList<>();
+        list.add( new Students(19, "Oppie", 132)); 
+        list.add( new Students(20, "Nancy", 129));
+        Collections.sort(list, new SortByMarks()); 
+        System.out.println(list); 
 
-        
-
-    }
+}
 }
