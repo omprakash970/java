98c86673e0b1cd67a0167e07c9e763900a7c6569
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-21 21:14:40 +0530

    done

--- collections/arraylist.java
+++ collections/arraylist.java
@@ -1,4 +1,4 @@
-package OOP;
+package collections;
 import java.util.ArrayList;
 public class arraylist {
     public static void main(String[] args) {
@@ -9,7 +9,11 @@ public class arraylist {
         System.out.println("ArrayList: " + list);
         list.removeLast();
         list.removeFirst();
+        list.add(1, 150);
+        list.set(1, 175);
         System.out.println("After removing first and last elements: " + list);
+
+        System.out.println("Element at index 1:"+ list.get(1));
 
     }
 
---
+++ collections/linkedlist.java
@@ -0,0 +1,18 @@
+package collections;
+import java.util.LinkedList;
+import java.util.Iterator;
+class Linkedlist {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(10); 
+        list.add(20);
+        list.add(30);
+            
+        list.addFirst(10);
+        list.addLast(40);
+        list.get(0);
+        
+    }
+
+    
+}
